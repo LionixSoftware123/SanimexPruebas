50f8a04bbd7b1eba4ed6051428c9d3f9d9226a7c
axiskg <84153791+axiskg@users.noreply.github.com>
2025-10-22 08:44:28 -0600

    Update CheckoutShippingMethods.tsx
    Solo modifico la linea de las clases, ya que el cambio anterior de todo el botón generaba error al momento  de compilar

--- src/components/checkout/CheckoutShippingMethods.tsx
+++ src/components/checkout/CheckoutShippingMethods.tsx
@@ -113,11 +113,7 @@ const CheckoutShippingMethods: React.FC<CheckoutShippingMethodsProps> = ({
                 onSelected(ShippingEnum.ByShipping, selectedShippingZone);
                 e.preventDefault();
               }}
-              className={` ${
-                (!postalCodeShipping.includes(parseInt(postalCode as string)) || !postalCodeShippingProvincia.includes(parseInt(postalCode as string)))
-                  ? 'hidden'
-                  : ' flex'
-              } rounded-full  border border-[#919191] w-[14px] h-[14px] mx-2 flex ml-8 mb-6 self-center items-start justify-start absolute `}
+              className="rounded-full border border-[#919191] w-[14px] h-[14px] mx-2 flex ml-8 mb-6 self-center items-start justify-start absolute"
             >
               <div
                 className={`${
