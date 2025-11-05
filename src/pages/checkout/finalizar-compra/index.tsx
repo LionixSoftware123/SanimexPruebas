91adbc2c778c3dc1908969f0da250361799b3940
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-05 10:26:28 -0600

    Update index.tsx
    Regreso a verificar si el user ya existe

--- src/pages/checkout/finalizar-compra/index.tsx
+++ src/pages/checkout/finalizar-compra/index.tsx
@@ -5,12 +5,12 @@ import InputMask from 'react-input-mask';
 import { useToasts } from 'react-toast-notifications';
 import cardValidator from 'card-validator';
 import {
-  //UserNodeIdTypeEnum,
+  UserNodeIdTypeEnum,
   InternalBannerResponse,
 } from '@/utils/types/generated';
-//import { fetchUser } from '@/modules/auth/auth-actions';
+import { fetchUser } from '@/modules/auth/auth-actions';
 import Link from 'next/link';
-//import { FRONTEND_ENDPOINT } from '@/utils/constants';
+import { FRONTEND_ENDPOINT } from '@/utils/constants';
 import { useUserHook } from '@/modules/auth/user-hooks';
 import { ShippingEnum } from '@/components/checkout/CheckoutShippingMethods';
 import {
@@ -308,7 +308,7 @@ const Data: React.FC<DataProps> = ({ internalBanner }) => {
       email: user ? user.email : data.billingAddress?.email,
     };
 
-    /**const checkUser = await fetchUser({
+    const checkUser = await fetchUser({
       idType: UserNodeIdTypeEnum.Email,
       id: billingInfo.email as string,
     });
@@ -330,7 +330,7 @@ const Data: React.FC<DataProps> = ({ internalBanner }) => {
           appearance: 'error',
         },
       );
-    }**/
+    }
 
     return await createBanortePayment(
       data.card,
