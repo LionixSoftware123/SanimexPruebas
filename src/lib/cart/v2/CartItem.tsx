65ff7afb017fe535bff1ea1d753412d7def5fed5
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-07 00:03:48 -0600

    Update CartItem.tsx
    pongo la ruta completa de topbanner

--- src/lib/cart/v2/CartItem.tsx
+++ src/lib/cart/v2/CartItem.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { CartItem, CartItemVariation } from './cart-types';
 import { formatCurrency } from './utils/formats';
 import { addCartAction, removeCartAction } from '@/lib/cart/v2/cart-actions';
-import { renderTopBannerEvent } from '@/modules/banner/banner-events';
+import { renderTopBannerEvent } from 'src/modules/banner/banner-events.ts';
 
 import { useCartHook } from './cart-hooks';
 import { useToasts } from 'react-toast-notifications';
