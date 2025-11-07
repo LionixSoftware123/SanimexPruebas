142f04ef12fca4e60ad4c2113b0b4a307dcad52a
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-07 11:42:33 -0600

    Update CheckoutShippingMethods.tsx
    agrego console.log para ver el respon de la api

--- src/components/checkout/CheckoutShippingMethods.tsx
+++ src/components/checkout/CheckoutShippingMethods.tsx
@@ -73,6 +73,8 @@ const CheckoutShippingMethods: React.FC<CheckoutShippingMethodsProps> = ({
   const handleShippingZone = async () => {
     const response = await axios.get('/api/woo-shipping-zones');
 
+    console.log( response.data );
+
     setShippingZone(response.data);
   };
 
