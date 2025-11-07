e26dfed71ad80019ba140a28703e4e1fa65dbbfd
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-07 14:10:25 -0600

    Update index.tsx
    console.log para shop

--- src/pages/checkout/finalizar-compra/index.tsx
+++ src/pages/checkout/finalizar-compra/index.tsx
@@ -281,7 +281,7 @@ const Data: React.FC<DataProps> = ({ internalBanner }) => {
   // }, [cart]);
 
   //console.log({ cart });
-  //console.log({ userData });
+  console.log({ shop });
 
   const handleOrderCompletion = (orderId: number | undefined) => {
     setOrderId(orderId);
