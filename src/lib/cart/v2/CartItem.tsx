de7dd976fac36ee23fb3bb8f0ff30b9a64e44673
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-06 23:16:21 -0600

    Update CartItem.tsx
    se tenía mal el cierre del div

--- src/lib/cart/v2/CartItem.tsx
+++ src/lib/cart/v2/CartItem.tsx
@@ -119,17 +119,10 @@ const CartItemComponent: React.FC<CartItemProps> = ({ cartItem }) => {
               {cartItem?.prices?.price !== cartItem?.prices?.regular_price ? (
                 <>
                  <div className="col-span-2 text-[#111111] font-Century-Gothic">
-                    <div className="line-through text-[#666666] text-[12px]">
-                    <div
-                    className="col-span-2 text-white text-[12px] rounded min-w-[50px] h-[20px] px-[5px]"
-                    style={{ backgroundColor: topBanner.color as string }}
-                    >
+                    <div className="col-span-2 text-white text-[12px] rounded min-w-[50px] h-[20px] px-[5px]" style={{ backgroundColor: topBanner.color as string }}>
                       antes {formatCurrency(cartItem?.prices?.regular_price)} MXN
                     </div>
-                    <div
-                    className="col-span-2 text-white text-[12px] rounded min-w-[50px] h-[20px] px-[5px]"
-                    style={{ backgroundColor: topBanner.color as string }}
-                    >
+                    <div className="col-span-2 text-white text-[12px] rounded min-w-[50px] h-[20px] px-[5px]" style={{ backgroundColor: topBanner.color as string }}>
                       con descuento {formatCurrency(cartItem?.prices?.price)} MXN
                     </div>
                  </div>
