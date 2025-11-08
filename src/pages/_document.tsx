3a2538bfe0992b54d4907f46a1c2fc69f6929004
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-07 21:44:22 -0600

    Update _document.tsx
    no queda documents.tsx si quito los scripts, a ver asi

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
-//import { Html, Head, Main, NextScript } from 'next/document';
-import { Html, Head, Main } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 //import Script from 'next/script';
 import React from 'react';
 
@@ -29,8 +28,8 @@ export default function Document() {
           }}
         ></noscript>**/}
         <Main />
-        {/**<NextScript />
-        <Script
+        <NextScript />
+        {/**<Script
           async
           strategy="beforeInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-BZSVMDZGGK"
@@ -51,7 +50,6 @@ export default function Document() {
             __html: `    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
     vgo('setAccount', '69083262');
     vgo('setTrackByDefault', true);
-
     vgo('process');`,
           }}
         ></Script>**/}
