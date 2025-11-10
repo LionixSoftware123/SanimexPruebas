918e467e71114c903391f00a51d79958582316d1
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-09 18:25:06 -0600

    Update _document.tsx
    Agrgeo el unico script que no es de GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-//import Script from 'next/script';
+import Script from 'next/script';
 import React from 'react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Este es el tipo de script a reubicar/reemplazar */}
-        {/* <script src="https://example.com/external-script.js"></script> */}
+        <Script
+          id="googleGoogleTagId"
+          async
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+            vgo('setAccount', '69083262');
+            vgo('setTrackByDefault', true);
+            vgo('process');`,
+          }}
+        ></Script>
       </Head>
       <body>
         <Main />
