050da8915e6d38eb90fb8e8cea6e587078b51988
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-10 14:57:53 -0600

    Update index.tsx
    Le regreso como estaba

--- src/pages/carrito/index.tsx
+++ src/pages/carrito/index.tsx
@@ -81,9 +81,9 @@ const CartPage: React.FC<CartPageProps> = ({ internalBanner }) => {
               <>
                 {cart && cart?.items && cart?.items?.length ? (
                   <div className="grid grid-cols-12 gap-4 mt-24">
-                    <div className="col-span-full md:col-span-10 overflow-x-auto">
+                    <div className="col-span-full md:col-span-12 overflow-x-auto">
                       <div className="overflow-x-auto">
-                        <div className="overflow-x-auto">
+                        <div className="grid grid-cols-12 w-[850px] lg:w-full gap-4 border-[#C1C1C1] border-b pb-4">
                           <div className="col-span-4">
                             <div className="text-[#1C355E] text-[16px] font-Century-Gothic-Bold">
                               Producto
@@ -120,7 +120,7 @@ const CartPage: React.FC<CartPageProps> = ({ internalBanner }) => {
                             ))}
                         </div>
                       </div>
-                      <div className="text-end mb-6">
+                      <div className="col-span-full text-end mb-6">
                         <button
                           className="inline-flex text-[#F17523] text-[12px]"
                           onClick={() =>
@@ -173,7 +173,7 @@ const CartPage: React.FC<CartPageProps> = ({ internalBanner }) => {
                       </div>
                     </div>
 
-                    <div className="col-span-full">
+                    <div className="col-span-full md:col-span-12">
                       <CartDetails cart={cart} />
                       <div className="mb-6">
                         {internalBanner?.redirectUrl &&
@@ -196,7 +196,7 @@ const CartPage: React.FC<CartPageProps> = ({ internalBanner }) => {
                     </div>
 
                     {upsell && upsell.length ? (
-                      <div className="col-span-full">
+                      <div className="col-span-full md:col-span-12">
                         <div className="text-[20px] font-Century-Gothic mb-4">
                           Materiales de instalación necesarios
                         </div>
