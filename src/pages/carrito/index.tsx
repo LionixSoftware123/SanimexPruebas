d060d8e5c9b89017f17d08998a0a51a500449cab
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-10 17:21:58 -0600

    Update index.tsx
    Cambio el div princpla que no sea grid

--- src/pages/carrito/index.tsx
+++ src/pages/carrito/index.tsx
@@ -80,7 +80,7 @@ const CartPage: React.FC<CartPageProps> = ({ internalBanner }) => {
             {!loading ? (
               <>
                 {cart && cart?.items && cart?.items?.length ? (
-                  <div className="grid grid-cols-12 gap-4 mt-24">
+                  <div className="col-span-full mt-24">
                     <div className="col-span-full md:col-span-12 overflow-x-auto">
                       <div className="overflow-x-auto">
                         <div className="grid grid-cols-12 w-[850px] lg:w-full gap-4 border-[#C1C1C1] border-b pb-4">
