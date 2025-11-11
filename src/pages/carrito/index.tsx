efbce5b602692455586b2125bd27d8eb5c4da9a3
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-10 22:31:53 -0600

    Update index.tsx
    Modifico para que el resumen del gatos se alinea a la derecha

--- src/pages/carrito/index.tsx
+++ src/pages/carrito/index.tsx
@@ -173,7 +173,7 @@ const CartPage: React.FC<CartPageProps> = ({ internalBanner }) => {
                       </div>
                     </div>
 
-                    <div className="col-span-full md:col-span-12">
+                    <div style="justify-items: end;">
                       <CartDetails cart={cart} />
                       <div className="mb-6">
                         {internalBanner?.redirectUrl &&
