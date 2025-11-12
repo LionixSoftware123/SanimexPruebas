6c372b0bbb17166a907c5fc5b46013901bf9ea37
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-11 18:01:37 -0600

    Update index.tsx
    A 4 columnas el siguiente bloque

--- src/pages/checkout/pago-completado/index.tsx
+++ src/pages/checkout/pago-completado/index.tsx
@@ -263,7 +263,7 @@ const PaymentComplete: React.FC<PaymentCompleteProps> = ({
                       </p>
                     </div>
                   </div>
-                  <div className="grid grid-cols-3 my-14">
+                  <div className="grid grid-cols-4 my-14">
                     <div className="border-r border-r-[#C1C1C1] p-2">
                       <p className="pb-2 uppercase text-[#636464] font-Century-Gothic-Bold text-center">
                         Banco:
