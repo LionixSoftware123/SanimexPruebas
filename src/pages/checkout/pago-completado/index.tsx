b83f647a89586f90d4939cb7d9a8348b7665dea0
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-14 13:11:19 -0600

    Update index.tsx
    pongo el código ascci correcto en el texto del WA

--- src/pages/checkout/pago-completado/index.tsx
+++ src/pages/checkout/pago-completado/index.tsx
@@ -280,7 +280,7 @@ const PaymentComplete: React.FC<PaymentCompleteProps> = ({
                         A nombre de:
                       </p>
                       <p className="pb-2 font-Century-Gothic text-[12px] text-center">{NameDetails}</p>
-                      <p className="flex self-center font-Century-Gothic text-[10px] text-center justify-center items-center">Atención a clientes: <a href={`https://api.whatsapp.com/send?phone=${WhatsApp}&text=¿Tienes dudas? Escríbenos, estamos para ayudarte
+                      <p className="flex self-center font-Century-Gothic text-[10px] text-center justify-center items-center">Atención a clientes: <a href={`https://api.whatsapp.com/send?phone=${WhatsApp}&text=¿Tienes%20dudas?%20Escríbenos,%20estamos%20para%20ayudarte
 `} className=" flex self-center ml-1 w-[20px] h-[20px] md:w-[20px] md:h-[20px] drop-shadow-lg pointer-events-auto"
           target="_blank"><IconWhatsapp /></a></p>
                     </div>
