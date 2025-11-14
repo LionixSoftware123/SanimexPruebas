4f669b8bf46e2d15357a2b3a98cb63fa8553dfe7
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-13 19:09:33 -0600

    Update index.tsx
    Reduzco el tamaño de letra de AC

--- src/pages/checkout/pago-completado/index.tsx
+++ src/pages/checkout/pago-completado/index.tsx
@@ -280,8 +280,8 @@ const PaymentComplete: React.FC<PaymentCompleteProps> = ({
                         A nombre de:
                       </p>
                       <p className="font-Century-Gothic text-[12px] text-center">{NameDetails}</p>
-                      Atención a clientes: <a href={`https://api.whatsapp.com/send?phone=${WhatsApp}`} className=" flex self-center ml-1 w-[30px] h-[30px] md:w-[30px] md:h-[30px] drop-shadow-lg pointer-events-auto"
-          target="_blank"><IconWhatsapp /></a>
+                      <p className="font-Century-Gothic text-[10px] text-center">Atención a clientes: <a href={`https://api.whatsapp.com/send?phone=${WhatsApp}`} className=" flex self-center ml-1 w-[30px] h-[30px] md:w-[30px] md:h-[30px] drop-shadow-lg pointer-events-auto"
+          target="_blank"><IconWhatsapp /></a></p>
                     </div>
                   </div>
              
