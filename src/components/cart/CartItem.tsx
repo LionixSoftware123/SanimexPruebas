ad8f8892f1e2ea0cc1156bb8229c3593d3b196ba
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-19 14:20:59 -0600

    Update CartItem.tsx
    Regreso como estaba

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -15,11 +15,8 @@ import { updateCartItemsStore } from '@/modules/cart/cart-events';
 import Link from 'next/link';
 import { getImageAttributes } from '@/utils/utils';
 
-/**const ProductInputQuantity = dynamic(
-  () => import('@/components/product/components/ProductInputQuantity'),
-);**/
 const ProductInputQuantity = dynamic(
-  () => import('@/lib/cart/v2/components/ProductInputQuantity'),
+  () => import('@/components/product/components/ProductInputQuantity'),
 );
 
 type CartItemProps = {
