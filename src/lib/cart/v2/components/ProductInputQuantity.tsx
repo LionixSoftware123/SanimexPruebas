48033e2009ea78b7462725b2bf9691452f271d0b
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-20 16:44:30 -0600

    Update ProductInputQuantity.tsx
    Quito el console.log para no confundirme.

--- src/lib/cart/v2/components/ProductInputQuantity.tsx
+++ src/lib/cart/v2/components/ProductInputQuantity.tsx
@@ -16,7 +16,6 @@ const ProductInputQuantity: React.FC<ProductInputQuantityProps> = ({
   maxQuantity,
 }) => {
   const { addToast } = useToasts();
-  console.log( quantity );
   return (
     <div className="self-center  w-[111px] flex border border-[#838383] py-[5px] min-w-[120px] rounded-[5px] justify-center items-center">
       <button
