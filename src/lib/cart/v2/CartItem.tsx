ce6e6fd3ec632ef00adec3ffd85aca223b0fdef8
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-20 18:23:59 -0600

    Update CartItem.tsx
    Regreso el texto de removeCartAction, ya que no es el mensaje que sale.

--- src/lib/cart/v2/CartItem.tsx
+++ src/lib/cart/v2/CartItem.tsx
@@ -35,7 +35,7 @@ const CartItemComponent: React.FC<CartItemProps> = ({ cartItem }) => {
       // });
     },
     onError: () => {
-      addToast('Tenemos problemas para agregar el producto! solo ver si es este', {
+      addToast('Tenemos problemas para agregar el producto!', {
         appearance: 'error',
       });
     },
