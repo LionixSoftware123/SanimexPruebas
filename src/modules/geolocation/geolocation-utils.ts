326e2d1c2d6f6a44a1802b7c014858e2777428c5
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-26 11:31:47 -0600

    Update geolocation-utils.ts
    Agrego toFixed(2) al distance.value

--- src/modules/geolocation/geolocation-utils.ts
+++ src/modules/geolocation/geolocation-utils.ts
@@ -66,7 +66,7 @@ export const calculateCost = (
 ) => {
   if (freeShipping) return 0;
   if (distance && distance.value >= 10000)
-    return Math.round((COST_FIXED + ((distance.value - 10000) / 1000) * COST_BY_KM) * 100)/100;
+    return Math.round((COST_FIXED + ((distance.value.toFixed(2) - 10000) / 1000) * COST_BY_KM) * 100)/100;
   if (distance) return COST_FIXED;
   return 0;
 };
