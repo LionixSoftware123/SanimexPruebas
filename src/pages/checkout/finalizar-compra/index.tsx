a77f5d86a906758a4bc3527958178827ddb0e4a6
axiskg <84153791+axiskg@users.noreply.github.com>
2025-11-27 16:21:09 -0600

    Update index.tsx
    Después de convertirlo en numero lo hago parseFloat y lo divido entre 100

--- src/pages/checkout/finalizar-compra/index.tsx
+++ src/pages/checkout/finalizar-compra/index.tsx
@@ -474,7 +474,7 @@ const Data: React.FC<DataProps> = ({ internalBanner }) => {
   const postalCode = isShipping ? shippingPostalCodeForm : postalCodeForm;
 
   //const total = cart?.totals?.total_price ?? 0;
-  const total =  Number(cart?.totals?.total_price);
+  const total =  parseFloat(Number(cart?.totals?.total_price))/100;
   console.log( total );
 
   let content = <></>;
