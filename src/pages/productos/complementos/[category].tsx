3a82763e622daecd30f66cee95a95d45b5b9bd1d
axiskg <84153791+axiskg@users.noreply.github.com>
2025-12-01 10:55:42 -0600

    complementos
    Se modifica index como category TSX para ocultar diseño y medidas

--- src/pages/productos/complementos/[category].tsx
+++ src/pages/productos/complementos/[category].tsx
@@ -279,7 +279,7 @@ const ProductsPage: React.FC = () => {
                 />
                 ) : null}
                 
-                {designs.length > 0 && visible ? (
+                {designs.length > 0 && !visible ? (
                 <EasySearchAttributeList
                   selected={router.query.design as string}
                   items={designs}
@@ -291,7 +291,7 @@ const ProductsPage: React.FC = () => {
                 />
                 ) : null}
                 
-                {measures.length > 0 && visible ? (
+                {measures.length > 0 && !visible ? (
                 <EasySearchAttributeList
                   selected={router.query.measure as string}
                   items={measures}
