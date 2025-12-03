c1d7162cb8696b7dfb549f77c6e5d287e126ee34
axiskg <84153791+axiskg@users.noreply.github.com>
2025-12-02 19:43:54 -0600

    Update formats.ts
    Agrego la funcion formatCurrency2

--- src/lib/cart/v2/utils/formats.ts
+++ src/lib/cart/v2/utils/formats.ts
@@ -13,6 +13,21 @@ export const formatCurrency = (value: string | number): string => {
   }).format(adjustedValue);
 };
 
+export const formatCurrency2 = (value: string | number): string => {
+  const numericValue =
+    typeof value === 'string' ? parseFloat(value.replace(/,/g, '')) : value;
+
+  if (isNaN(numericValue)) {
+    return '$0.00';
+  }
+  const adjustedValue = numericValue;
+
+  return new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+  }).format(adjustedValue);
+};
+
 export const decodeHtmlEntities = (text: string) => {
   const parser = new DOMParser();
   const decodedString = parser.parseFromString(text, 'text/html')
