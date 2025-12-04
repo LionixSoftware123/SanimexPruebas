409b60883c7f7fa2dab4dec6ee9169df92b43f75
axiskg <84153791+axiskg@users.noreply.github.com>
2025-12-04 17:02:00 -0600

    Update CheckoutShippingMethods.tsx
    Lo regreso como estaba

--- src/components/checkout/CheckoutShippingMethods.tsx
+++ src/components/checkout/CheckoutShippingMethods.tsx
@@ -115,7 +115,7 @@ const CheckoutShippingMethods: React.FC<CheckoutShippingMethodsProps> = ({
                 e.preventDefault();
               }}
               className={` ${
-                !(postalCodeShipping.includes(parseInt(postalCode as string)) || postalCodeShippingProvincia.includes(parseInt(postalCode as string)) || postalCodeShippingProvinciaNL.includes(parseInt(postalCode as string)))
+                !(postalCodeShipping.includes(parseInt(postalCode as string)) || postalCodeShippingProvincia.includes(parseInt(postalCode as string)))
                   ? 'hidden'
                   : ' flex'
               } rounded-full  border border-[#919191] w-[14px] h-[14px] mx-2 flex ml-8 mb-6 self-center items-start justify-start absolute `}
