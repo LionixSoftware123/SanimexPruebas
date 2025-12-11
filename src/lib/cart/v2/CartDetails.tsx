d3f7f04b6c4065d4590d629e74ee883587af5587
axiskg <84153791+axiskg@users.noreply.github.com>
2025-12-11 16:26:03 -0600

    Update CartDetails.tsx
    Agrego console.log(currentStep);

--- src/lib/cart/v2/CartDetails.tsx
+++ src/lib/cart/v2/CartDetails.tsx
@@ -24,6 +24,7 @@ const CartDetails: React.FC<CartDetailsProps> = ({ cart }) => {
   const router = useRouter();
 
   console.log(AuthSteps);
+  console.log(currentStep);
 
   switch (currentStep) {
     case AuthSteps.Auth:
