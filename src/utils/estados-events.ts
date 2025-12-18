12133e3e691f8d252dd3d44042c329300ccbef07
axiskg <84153791+axiskg@users.noreply.github.com>
2025-12-18 16:05:13 -0600

    Delete src/utils/estados-events.ts
    Se elimina estados-events., ya que no se necesitará

--- src/utils/estados-events.ts
+++
@@ -1,8 +0,0 @@
-import { createState } from '@cobuildlab/react-simple-state';
-import { EstadosType } from '@/utils/estados-types';
-
-export const selectedState = createState<{ estado: EstadosType | undefined }>({
-  initialValue: {
-    estado: undefined,
-  },
-});
