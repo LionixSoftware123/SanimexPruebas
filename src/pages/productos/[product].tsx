524d2deb4bed08870d9cb4bfea165ece432e49b7
axiskg <84153791+axiskg@users.noreply.github.com>
2026-02-05 02:25:03 -0600

    Update [product].tsx
    Quito el Container del cierre comercial

--- src/pages/productos/[product].tsx
+++ src/pages/productos/[product].tsx
@@ -236,11 +236,9 @@ const ProductPage: React.FC<ProductPageProps> = ({
             <ProductDescription product={product} />
           </Container>
           {product?.cierreComercial ? (
-            <Container classes="mb-6">
             <div className="grid grid-cols-12">
-              <div className=" pb-4 text-[14px] md:text-[17px] font-Century-Gothic-Bold text-[#9BA0C9]">{product?.cierreComercial}</div>
+              <div className=" text-[14px] md:text-[17px] font-Century-Gothic-Bold text-[#000000]">{product?.cierreComercial}</div>
             </div>
-            </Container>
           ) : (
            ''
           )}
