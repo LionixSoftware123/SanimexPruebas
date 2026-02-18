837a48a1f2b812dc76be8225ceb0e0869096e7a6
axiskg <84153791+axiskg@users.noreply.github.com>
2026-02-18 12:09:23 -0600

    Update GeolocationDialog.tsx
    agrego setCanConfirm(false); para deshabilitar el botónuna vez ejecutado el claculo

--- src/components/geolocation/GeolocationDialog.tsx
+++ src/components/geolocation/GeolocationDialog.tsx
@@ -77,8 +77,8 @@ const GeolocationDialog: React.FC<GeolocationDialogProps> = ({
 
   // Acción que se ejecuta al hacer clic
   const handleAction = (origin: any) => {
-    // Aquí puedes llamar a tu API en Node.js, ej: axios.post('/api/accion')
     callCheckStore(origin);
+    setCanConfirm(false);
   };  
 
   // useEffect se activa cuando 'isDisabled' cambia
