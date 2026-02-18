d0a1dc2e4bebf2054d9e45f032a26825a7e25cd3
axiskg <84153791+axiskg@users.noreply.github.com>
2026-02-18 10:39:42 -0600

    Update GeolocationDialog.tsx
    quito los comentarios de disabled botón para qu eno me de error de que la variable nunca se ha usado

--- src/components/geolocation/GeolocationDialog.tsx
+++ src/components/geolocation/GeolocationDialog.tsx
@@ -211,7 +211,7 @@ const GeolocationDialog: React.FC<GeolocationDialogProps> = ({
                             km
                           </div>
                           <button
-                            //disabled={canCalculate}
+                            disabled={canCalculate}
                             className="border border-[#1C355E] disabled:hover:bg-transparent disabled:hover:text-[#1C355E] disabled:cursor-pointer hover:bg-[#0033A1] hover:text-white mb-4 lg:mb-0 lg:mr-2 rounded-[5px] bg-white  h-[45px] flex items-center text-[#1C355E] text-[12px] px-8"
                             onClick={() => callCheckStore(origin)}
                           >
