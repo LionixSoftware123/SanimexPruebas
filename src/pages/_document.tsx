d7baa12b5475a3312171f736e68886cf4b66d37a
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-04 10:12:06 -0600

    Update _document.tsx
    Modifico la etsrategia del script a afterInteractive

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default function Document() {
         <Script
           id="googleGoogleTagId"
           async
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
     vgo('setAccount', '69083262');
