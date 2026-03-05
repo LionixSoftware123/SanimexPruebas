74248a7a5a0d782a6fd773121f728ae8fb9ee4f5
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-05 17:40:41 -0600

    Update _document.tsx
    Cambio sugerido para evitar el error en los productos simples

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 import React from 'react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
-          id="securepubads"
-          strategy="afterInteractive"
-          async
+        {/* USAMOS ETIQUETA <script> ESTÁNDAR DE HTML */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -17,20 +14,20 @@ export default function Document() {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NTLX9QV');`,
           }}
-        ></Script>
+        />
       </Head>
       <body>
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-      <iframe src='https://www.googletagmanager.com/ns.html?id=GTM-NTLX9QV'
-      height='0' width='0' style='display:none;visibility:hidden'></iframe>`,
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTLX9QV"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
           }}
-        ></noscript>
+        />
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
-
