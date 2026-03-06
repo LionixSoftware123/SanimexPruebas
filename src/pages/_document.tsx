a87eb7d52308caaf46611ee35ff9ba86fc1920fb
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-05 19:15:39 -0600

    Update _document.tsx
    Modificando de acuerdo a la sugerencia

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+//import Script from 'next/script';
 import React from 'react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
-          id="securepubads"
-          strategy="beforeInteractive"
-          async
+        {/* Google Tag Manager - Script base (Corregido a etiqueta nativa) */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -23,37 +21,25 @@ export default function Document() {
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-      <iframe src='https://www.googletagmanager.com/ns.html?id=GTM-NTLX9QV'
-      height='0' width='0' style='display:none;visibility:hidden'></iframe>`,
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTLX9QV"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
           }}
-        ></noscript>
+        />
         <Main />
         <NextScript />
-        <Script
-          async
-          strategy="beforeInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-BZSVMDZGGK"
-        ></Script>
-        <Script
-          id="googleGoogleTagId"
-          async
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'G-BZSVMDZGGK');`,
-          }}
-        ></Script>
-        <Script
-          id="googleGoogleTagId"
-          async
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
-    vgo('setAccount', '69083262');
-    vgo('setTrackByDefault', true);
 
-    vgo('process');`,
+        {/* Script de ActiveCampaign / vgo (Corregido a etiqueta nativa) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+              vgo('setAccount', '69083262');
+              vgo('setTrackByDefault', true);
+              vgo('process');
+            `,
           }}
-        ></Script>
+        />
       </body>
     </Html>
   );
