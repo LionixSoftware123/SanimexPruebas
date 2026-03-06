46df360909cf54b7c243e477c1e21aa4dd4f741a
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-05 18:07:15 -0600

    upload file
    Se suven la smodificaciones sugeridas en documents y app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* USAMOS ETIQUETA <script> ESTÁNDAR DE HTML */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NTLX9QV');`,
-          }}
-        />
+        {/* Aquí NO ponemos scripts dinámicos de Next.js */}
       </Head>
       <body>
+        {/* El NoScript de GTM debe ir siempre al inicio del body */}
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
