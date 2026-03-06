7d648eb9eed62a2553a07608ffbe7d2220937c1f
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-05 18:14:26 -0600

    Update _app.tsx
    Devuelvo como estaba

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,12 @@ import { ToastProvider } from 'react-toast-notifications';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import NextNProgress from 'nextjs-progressbar';
 import Head from 'next/head';
-import Script from 'next/script'; // <--- IMPORTANTE: Importar desde next/script
 import { GOOGLE_API_KEY } from '@/utils/constants';
 import { useRouter } from 'next/router';
 import UserProvider from '@/modules/user/UserProvider';
 import { ProductCompareProvider } from '@/components/product/ProductCompareProvider';
 import { OnTokenEvent } from '@/modules/auth/auth-events';
 
-// ... (tus imports dinámicos se mantienen igual)
 const ActiveCampaignProvider = dynamic(
   () => import('@/components/active-campaign/ActiveCampaignProvider'),
   { ssr: false },
@@ -32,7 +30,17 @@ export default function App({ Component, pageProps }: AppProps) {
   const [cookies] = useCookies(['jwtAuthToken']);
   const router = useRouter();
 
-  // ... (tus useEffect se mantienen igual)
+  useEffect(() => {
+    if (router.asPath === '/mi-cuenta/lost-password') {
+      router.push('/auth');
+    }
+  }, [router]);
+
+  useEffect(() => {
+    if (cookies.jwtAuthToken) {
+      OnTokenEvent.dispatch({ token: cookies.jwtAuthToken });
+    }
+  }, [cookies]);
 
   return (
     <>
@@ -41,24 +49,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="robots" content="index, follow" />
         <meta charSet="utf-8" />
       </Head>
-
-      {/* --- GOOGLE TAG MANAGER CONFIGURACIÓN CORRECTA --- */}
-      <Script
-        id="gtm-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NTLX9QV');
-          `,
-        }}
-      />
-
       <GoogleOAuthProvider clientId={GOOGLE_API_KEY as string}>
-        <ToastProvider placement="top-right" autoDismiss autoDismissTimeout={5000}>
+        <ToastProvider
+          placement="top-right"
+          autoDismiss
+          autoDismissTimeout={5000}
+        >
           <ApolloProvider client={client}>
             <ProductCompareProvider>
               <NextNProgress options={{ showSpinner: false }} />
