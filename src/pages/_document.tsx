5097628ead306f02f2582aca4cd86e416a53a592
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-05 18:22:31 -0600

    Update _document.tsx
    Regreso todo como estaba

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,40 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import React from 'react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Aquí NO ponemos scripts dinámicos de Next.js */}
       </Head>
       <body>
-        {/* El NoScript de GTM debe ir siempre al inicio del body */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTLX9QV"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
         <Main />
         <NextScript />
+        <Script
+          async
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-BZSVMDZGGK"
+        ></Script>
+        <Script
+          id="googleGoogleTagId"
+          async
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'G-BZSVMDZGGK');`,
+          }}
+        ></Script>
+        <Script
+          id="googleGoogleTagId"
+          async
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+    vgo('setAccount', '69083262');
+    vgo('setTrackByDefault', true);
+
+    vgo('process');`,
+          }}
+        ></Script>
       </body>
     </Html>
   );
