dc09c6e14e7e5fb3c5950fe68489d4ca660b388a
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-11 17:02:35 -0600

    Update ProductDescription.tsx
    Se pone el borde en todos, seleccionado o no

--- src/components/product/components/ProductDescription.tsx
+++ src/components/product/components/ProductDescription.tsx
@@ -170,7 +170,7 @@ const ProductDescription: React.FC<ProductDescriptionProps> = ({ product }) => {
           className={`flex items-center  ${
             option === 1
               ? 'border-[#B2B2B2] border-l border-r border-t text-[#0033A1]'
-              : ' text-[#000] border-l-res border-r-res border-t border-[#B2B2B2]'
+              : ' text-[#000] border-l border-r border-t border-[#B2B2B2]'
           } p-2 justify-center `}
         >
           Información Adicional
@@ -182,7 +182,7 @@ const ProductDescription: React.FC<ProductDescriptionProps> = ({ product }) => {
             className={`flex items-center  ${
               option === 2
                 ? 'border-[#B2B2B2] border-l border-r border-t text-[#0033A1]'
-                : ' text-[#000] border-l-res border-r-res border-t border-[#B2B2B2]'
+                : ' text-[#000] border-l border-r border-t border-[#B2B2B2]'
             } p-2 justify-center `}
           >
             Descripción Técnica
@@ -197,7 +197,7 @@ const ProductDescription: React.FC<ProductDescriptionProps> = ({ product }) => {
             className={`flex items-center  ${
               option === 3
                 ? 'border-[#B2B2B2] border-l border-r border-t text-[#0033A1]'
-                : ' text-[#000] border-l-res border-r-res border-t border-[#B2B2B2]'
+                : ' text-[#000] border-l border-r border-t border-[#B2B2B2]'
             } p-2 justify-center `}
           >
             Descripción del producto
@@ -212,7 +212,7 @@ const ProductDescription: React.FC<ProductDescriptionProps> = ({ product }) => {
             className={`flex items-center  ${
               option === 4
                 ? 'border-[#B2B2B2] border-l border-r border-t text-[#0033A1]'
-                : ' text-[#000] border-l-res border-r-res border-t border-[#B2B2B2]'
+                : ' text-[#000] border-l border-r border-t border-[#B2B2B2]'
             } p-2 justify-center `}
           >
             Beneficios principales, Usos y Aplicaciones
