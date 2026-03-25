03f1c5ad4e6a742cc9f0f6c9d518b75d7a0c2ef5
axiskg <84153791+axiskg@users.noreply.github.com>
2026-03-25 16:26:06 -0600

    Update Schema.tsx
    Cambio el scheman al sugerido

--- src/components/utils/Schema.tsx
+++ src/components/utils/Schema.tsx
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Head from 'next/head';
 import { checkProductInStock, getMarca } from '@/modules/product/product-utils';
-import {
-  Product,
-  SimpleProduct,
-  ProductTypesEnum,
-} from '@/utils/types/generated';
-import useScript, { HTMLPositionEnum } from '@/utils/useScript';
-import { removeScript } from '@/utils/ads-utils';
-import { useRouter } from 'next/router';
+import { Product, SimpleProduct } from '@/utils/types/generated';
+
 type SchemaProps = {
   product: Product;
   url?: string;
 };
 
 const Schema: React.FC<SchemaProps> = ({ product, url }) => {
-  const router = useRouter();
-  let schema: any = {
+  // Al venir ya limpio desde getStaticProps, solo aseguramos que sea String
+  const priceToUse = String((product as SimpleProduct)?.price || "0.00");
+
+  const schemaData = {
     '@context': 'https://schema.org',
     '@type': 'Product',
-    sku: (product.sku as string) ? (product.sku as string) : '',
-    image: product?.galleryImages?.edges.length
+    sku: product?.sku ? String(product.sku) : String(product?.databaseId || ''),
+    image: product?.galleryImages?.edges?.length
       ? product?.galleryImages.edges[0].node.sourceUrl
-      : product?.featuredImage?.node.sourceUrl,
-    name: product?.name as String,
-    description: product?.name as String,
+      : product?.featuredImage?.node?.sourceUrl,
+    name: product?.name || '',
+    description: product?.description 
+      ? product.description.replace(/<[^>]*>?/gm, '') // Limpia HTML para el SEO
+      : product?.name || '',
     brand: {
       '@type': 'Brand',
-      name: getMarca(product),
+      name: getMarca(product) || 'Sanimex',
     },
     mainEntityOfPage: {
       '@type': 'WebPage',
@@ -35,28 +34,22 @@ const Schema: React.FC<SchemaProps> = ({ product, url }) => {
     offers: {
       '@type': 'Offer',
       url: url,
+      priceCurrency: 'MXN',
+      price: priceToUse, // <--- "28502.19"
       itemCondition: 'https://schema.org/NewCondition',
       availability: checkProductInStock(product)
         ? 'https://schema.org/InStock'
-        : '',
+        : 'https://schema.org/OutOfStock',
       priceSpecification: {
         '@type': 'PriceSpecification',
-        price:
-          (product as SimpleProduct)?.type === ProductTypesEnum.Variable
-            ? (product as SimpleProduct).price
-                ?.split(' - ')[0]
-                .replaceAll(',', '')
-                .replaceAll('$', '')
-            : ((product as SimpleProduct)?.price as string)
-                .replaceAll(',', '')
-                .replaceAll('$', ''),
+        price: priceToUse,
         priceCurrency: 'MXN',
+        valueAddedTaxIncluded: true,
       },
       hasMerchantReturnPolicy: {
         '@type': 'MerchantReturnPolicy',
         applicableCountry: 'MX',
-        returnPolicyCategory:
-          'https://schema.org/MerchantReturnFiniteReturnWindow',
+        returnPolicyCategory: 'https://schema.org/MerchantReturnFiniteReturnWindow',
         merchantReturnDays: 60,
         returnMethod: 'https://schema.org/ReturnByMail',
         returnFees: 'https://schema.org/FreeReturn',
@@ -65,8 +58,7 @@ const Schema: React.FC<SchemaProps> = ({ product, url }) => {
         '@type': 'OfferShippingDetails',
         shippingRate: {
           '@type': 'MonetaryAmount',
-          minValue: 250,
-          maxValue: 500,
+          value: 0,
           currency: 'MXN',
         },
         shippingDestination: {
@@ -92,29 +84,16 @@ const Schema: React.FC<SchemaProps> = ({ product, url }) => {
     },
   };
 
-  useScript({
-    delay: 100,
-    scriptContent: `${JSON.stringify(schema)}`,
-    position: HTMLPositionEnum.Head,
-    scriptId: `schema-${url}`,
-    type: 'application/ld+json',
-  });
-  useEffect(() => {
-    const handleRouteChange = () => {
-      removeScript(`schema-${url}`);
-    };
-
-    router.events.on('routeChangeStart', () => {
-      handleRouteChange();
-    });
-
-    return () => {
-      router.events.off('routeChangeStart', () => {
-        handleRouteChange();
-      });
-    };
-  }, [router.events, url]);
-  return <></>;
+  return (
+    <Head>
+      <script
+        type="application/ld+json"
+        id={`schema-${url}`}
+        key={`jsonld-${url}`}
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+      />
+    </Head>
+  );
 };
 
 export default Schema;
